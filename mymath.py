a8e1a33be3a901b422ec7fcd4a5ac3e538952330
trantoan0212 <79898805+trantoan0212@users.noreply.github.com>
2021-05-29 22:21:59 +0700

    Add files via upload

---
+++ mymath.py
@@ -0,0 +1,19 @@
+import mymath
+def square (n):
+    return n*n
+def cube(n):
+    return n*n*n
+def average(values):
+    nvals=len(values)
+    sum = 0.0
+    for v in values:
+        sum+=v
+    return float(sum)/nvals
+values=[2,4,6,8,10]
+print('square:')
+for v in values:
+    print(mymath.square(v))
+print ('cubes:')
+for v in values:
+    print(mymath.cube(v))
+print('average: ' + str(mymath.average(values)))
